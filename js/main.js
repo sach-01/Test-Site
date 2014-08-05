a569cbf0ca42ab79b309d9b5594fd9654501d5ee
Sach <user@emperor.local>
2014-08-05 12:00:52 +0100

    My initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+﻿menu_status = new Array();
+
+function showHide(theid) {
+	if (document.getElementById) {
+		var switchId = document.getElementById(theid);
+		if (menu_status[theid] != 'show') {
+			switchId.className = 'show';
+			menu_status[theid] = 'show';
+		} else {
+			switchId.className = 'hide';
+			menu_status[theid] = 'hide';
+		}
+	}
+}
